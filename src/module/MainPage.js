1c55a7bcc3e72f9d79eae3cea47185343d49a447
Aduscias <ivan.melexov@gmail.com>
2018-11-18 05:36:06 +0300

    add api

--- src/module/MainPage.js
+++ src/module/MainPage.js
@@ -1,4 +1,3 @@
-
 //import logo from './logo.svg';
 //import ReactDOM from "react-dom";
 import '../App.css';
@@ -6,8 +5,8 @@ import React, {Component} from 'react';
 import {Row, Col, Button, Card} from 'antd';
 import Header from '../module/Header';
 import ApiLib from '../module/ApiLib';
-let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
+let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 
 class CardOrg extends Component {
@@ -15,29 +14,47 @@ class CardOrg extends Component {
         super(props);
         // Don't call this.setState() here!
         this.state = {
-            org: testOrg
+            org: [],
         };
-        //this.handleClick = this.handleClick.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.getOrgsList = this.getOrgsList.bind(this);
     }
-
-
-
 
     getOrgsList() {
 
         const tr = 'hey';
         let result = ApiLib.getOrgsList(tr)
             .then(response => {
-                this.setState ({org:response})
-                console.log(response)
+                //this.setState ({org:response})
+                console.log(response.org_names)
+                let new_org = {};
+                let new_org_arry=[]
+                for (let i=0;i<response.org_names.length;i++){
+                     new_org = {
+                        'id' : i,
+                        'title' : response.org_names[i],
+                        'description' : 'Тестовая организация'
+                    }
+                    new_org_arry.push(new_org);
+
+                }
+                //console.log(new_org);
+                //console.log(new_org_arry);
+                this.setState ({org:new_org_arry})
+                //let new_org= Object.assign({}, response.org_names);
             })
-
-
     }
+/*
+        id: 1,
+        title: "Спасите кошечек",
+        description: "Каждый кошке нужен дом, вы можете нам в этом помочь",
+ */
+
+
 
     handleClick() {
-        let res = this.getOrgsList();
-        console.log(res);
+        this.getOrgsList();
+        //console.log(res);
         /*
         console.log(this.state.org);
         this.setState({ org: testOrgNew });
@@ -45,10 +62,10 @@ class CardOrg extends Component {
     }
 
 
-   /* componentDidMount() {
-        let res = this.getOrgsList();
-        console.log('res:'+res);
-    }*/
+     componentDidMount() {
+         this.getOrgsList();
+      //   console.log('res:'+res);
+     }
 
 
     render() {
@@ -58,13 +75,13 @@ class CardOrg extends Component {
         }
 
 
-        function renderThreeOrg (orgsArr){
+        function renderThreeOrg(orgsArr) {
 
-            if (orgsArr.length == 3) {
+            if (orgsArr.length === 3) {
                 return (
                     <Row type="flex">
                         <Col span={8} id={orgsArr[0].id}>
-                            <Card title={orgsArr[0].title} extra={<a href="#">Подробнее</a>}>
+                            <Card title={orgsArr[0].title} extra={<a href="/Organization#test" > Подробнее</a>}>
                                 <p>{orgsArr[0].description}</p>
                             </Card>
                         </Col>
@@ -82,7 +99,7 @@ class CardOrg extends Component {
             }
             if (orgsArr.length == 2) {
                 return (
-                    <Row type="flex" >
+                    <Row type="flex">
                         <Col span={8} id={orgsArr[0].id}>
                             <Card title={orgsArr[0].title} extra={<a href="#">Подробнее</a>}>
                                 <p>{orgsArr[0].description}</p>
@@ -120,11 +137,11 @@ class CardOrg extends Component {
 
 
         let text = [];
-        for (let i=0; i<this.state.org.length; i+=3){
+        for (let i = 0; i < this.state.org.length; i += 3) {
             let cnt = [];
 
-            for (let j = i; j < Math.min(this.state.org.length, i+3); j++) {
-                cnt.push (this.state.org[j]);
+            for (let j = i; j < Math.min(this.state.org.length, i + 3); j++) {
+                cnt.push(this.state.org[j]);
             }
 
             console.log(cnt);
@@ -132,11 +149,10 @@ class CardOrg extends Component {
         }
 
 
-
         return (
             <div>
                 {text}
-                <Button type="primary" onClick={this.handleClick}> Поменяй Json!</Button>
+                <Button type="primary" onClick={this.handleClick}> Обновить список организаций</Button>
             </div>
         );
     }
@@ -150,8 +166,8 @@ class MainPage extends Component {
                 <ReactCSSTransitionGroup transitionName="example"
                                          transitionAppear={true} transitionAppearTimeout={500}
                                          transitionEnter={false} transitionLeave={false}>
-                    <Header />
-                    <CardOrg />
+                    <Header/>
+                    <CardOrg/>
                 </ReactCSSTransitionGroup>
             </div>
         );
@@ -159,85 +175,3 @@ class MainPage extends Component {
 }
 
 export default MainPage;
-
-let testOrg = [
-    {
-        id: 1,
-        title: "Спасите кошечек",
-        description: "Каждый кошке нужен дом, вы можете нам в этом помочь",
-        logo: "logo1.png",
-    },
-    {
-        id: 2,
-        title: "Собаки - наше все",
-        description: "Они врут, лучше всех в доме будет с собакой",
-        logo: "logo2.png",
-    },
-    {
-        id: 3,
-        title: "Что может быть лучше детей?",
-        description: "Дал Бог зайку, даст и лужайку. Сделайте что-то особенное",
-        logo: "logo3.png",
-    },
-    {
-        id: 4,
-        title: "Реабилитация программистов",
-        description: "Вы знаете как сложно блокчейн-программистам? Помогаем им социализироваться в реальном мире",
-        logo: "logo4.png",
-    }
-];
-
-
-
-let testOrgNew = [
-    {
-        id: 1,
-        title: "Новый новый ",
-        description: "Каждый кошке нужен дом, вы можете нам в этом помочь",
-        logo: "logo1.png",
-    },
-    {
-        id: 2,
-        title: "НОвый новый 2",
-        description: "<== Они врут, лучше всех в доме будет с собакой",
-        logo: "logo2.png",
-    },
-    {
-        id: 3,
-        title: "Что может быть лучше детей?",
-        description: "Дал Бог зайку, даст и лужайку. Сделайте что-то особенное",
-        logo: "logo3.png",
-    },
-];
-
-//console.log(testOrg);
-
-
-//console.log(renderOrg);
-//const template = Object.keys(data.books).map(item => <span key={data.books[item].id}>{data.books[item].author} - {data.books[item].name}</span>);
-
-
-/*
-// Parent Component
-class GroceryList extends Component {
-    render() {
-        return (
-            <ul>
-                <ListItem quantity="1" name="Bread" />
-                <ListItem quantity="6" name="Eggs" />
-                <ListItem quantity="2" name="Milk" />
-            </ul>
-        );
-    }
-}
-// Child Component
-class ListItem extends Component {
-    render() {
-        return (
-            <li>
-                {this.props.quantity} × {this.props.name}
-            </li>
-        );
-    }
-}
-*/
